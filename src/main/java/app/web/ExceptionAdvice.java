3dd712b1a951779cefb92405f7d18110fb83ca21
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-03-15 20:15:35 +0200

    Exception Handling: Exercise

--- src/main/java/app/web/ExceptionAdvice.java
+++ src/main/java/app/web/ExceptionAdvice.java
@@ -1,6 +1,7 @@
 package app.web;
 
 import app.web.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+@Hidden
 @ControllerAdvice
 public class ExceptionAdvice {
 
